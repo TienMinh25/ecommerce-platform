716f3a3a5a343e847b8ecc8945d69c7b4ea134aa
TuanMinh2512 <minhvipka2k3@gmail.com>
2025-03-24 22:56:36 +0700

    FEAT: add table role and permission

---
+++ internal/api-gateway/migrations/000004_add_resource_table_and_add_join_table_between_resources_and_permissions.up.down.sql
@@ -0,0 +1,3 @@
+DROP TRIGGER IF EXISTS set_timestamp_resources
+ON resources;
+DROP TABLE IF EXISTS resources;
---
+++ internal/api-gateway/migrations/000005_join_table_between_resources_and_permissions.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS permissions_resources(
+                                                    resource_id INTEGER REFERENCES roles(id) ON DELETE CASCADE,
+    permission_id INTEGER REFERENCES permissions(id) ON DELETE CASCADE,
+    PRIMARY KEY (resource_id, permission_id)
+    );
