22591f6f31dce90d4f11e1638726564ac6190edc
TienMinh25 <letienminh2512@gmail.com>
2025-04-16 07:59:09 +0700

    lam do

--- third_party/s3/minio.go
+++ third_party/s3/minio.go
@@ -4,24 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/TienMinh25/ecommerce-platform/internal/env"
-	"github.com/TienMinh25/ecommerce-platform/internal/utils"
 	"github.com/TienMinh25/ecommerce-platform/pkg"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/pkg/errors"
 	"log"
-	"net/http"
-	"strings"
 )
 
 type storage struct {
-	client     *minio.Client
-	bucketName string
-	urlPrefix  string
-	region     string
+	client        *minio.Client
+	defaultBucket string
+	region        string
+	tracer        pkg.Tracer
+	endpointURL   string
 }
 
-func NewStorage(env *env.EnvManager) (pkg.Storage, error) {
+func NewStorage(env *env.EnvManager, tracer pkg.Tracer) (pkg.Storage, error) {
 	useSSL := false
 
 	minioClient, err := minio.New(env.MinioConfig.MinioEndpointURL, &minio.Options{
@@ -37,7 +35,11 @@ func NewStorage(env *env.EnvManager) (pkg.Storage, error) {
 
 	ctx := context.Background()
 
-	isBucketExists, err := minioClient.BucketExists(ctx, env.MinioConfig.MinioBucketAvatars)
+	// Set default bucket
+	defaultBucket := env.MinioConfig.MinioBucketAvatars
+
+	// Check if default bucket exists and create if needed
+	isBucketExists, err := minioClient.BucketExists(ctx, defaultBucket)
 
 	if err == nil && !isBucketExists {
 		err = minioClient.MakeBucket(ctx, env.MinioConfig.MinioBucketAvatars, minio.MakeBucketOptions{
@@ -56,46 +58,42 @@ func NewStorage(env *env.EnvManager) (pkg.Storage, error) {
 		}
 	}
 
-	endpointURL := minioClient.EndpointURL()
-	urlPrefix := endpointURL.Scheme + "://" + env.MinioConfig.MinioBucketAvatars + ".s3." + env.MinioConfig.MinioRegion + "." + endpointURL.Host + "/"
-
 	return &storage{
-		client:     minioClient,
-		bucketName: env.MinioConfig.MinioBucketAvatars,
-		urlPrefix:  urlPrefix,
-		region:     env.MinioConfig.MinioRegion}, nil
+		client:        minioClient,
+		defaultBucket: defaultBucket,
+		region:        env.MinioConfig.MinioRegion,
+		tracer:        tracer,
+		endpointURL:   env.MinioConfig.MinioEndpointURL,
+	}, nil
 }
 
-// Delete implements pkg.Storage.
-func (s storage) Upload(ctx context.Context, payload pkg.UploadInput) (string, error) {
-	info, err := s.client.PutObject(ctx, s.bucketName, payload.Name, payload.File, payload.Size, minio.PutObjectOptions{
-		ContentType: payload.ContentType,
-	})
-
-	// if err exists, upload to minio fail
-	if err != nil {
-		return "", utils.TechnicalError{
-			Message: "Failed to upload object to S3",
-			Code:    http.StatusInternalServerError,
-		}
+// getBucketName returns the specified bucket or the default if none is specified
+func (s *storage) getBucketName(bucket string) string {
+	if bucket == "" {
+		return s.defaultBucket
 	}
 
-	return s.urlPrefix + info.Key, nil
+	return bucket
 }
 
-// Upload implements pkg.Storage.
-func (s storage) Delete(ctx context.Context, name string) error {
-	// get key object (get path of file on s3)
-	name = strings.TrimPrefix(name, s.urlPrefix)
+// getURLPrefix generates URL prefix for the given bucket
+func (s *storage) getURLPrefix(bucket string) string {
+	endpointURL := s.client.EndpointURL()
 
-	err := s.client.RemoveObject(ctx, s.bucketName, name, minio.RemoveObjectOptions{})
+	return fmt.Sprintf("%s://%s.s3.%s.%s/", endpointURL.Scheme, bucket, s.region, endpointURL.Host)
+}
 
-	if err != nil {
-		return utils.TechnicalError{
-			Message: "Failed to delete object on S3",
-			Code:    http.StatusInternalServerError,
-		}
-	}
+func (s *storage) Upload(ctx context.Context, payload pkg.UploadInput, bucket string) (string, error) {
+	ctx, span := s.tracer.StartFromContext(ctx, "Upload")
+	defer span.End()
+}
 
-	return nil
+func (s *storage) Delete(ctx context.Context, name string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *storage) GenerateUploadPresignedURL(ctx context.Context, objectName, bucket string) (string, error) {
+	//TODO implement me
+	panic("implement me")
 }
