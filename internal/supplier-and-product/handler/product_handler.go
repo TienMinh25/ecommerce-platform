25754371e3adc9cca26e3d95b4ce4edbfd3b0673
TienMinh25 <letienminh2512@gmail.com>
2025-05-07 12:30:04 +0700

    feat: setup handler and prepare dto for both service using

---
+++ internal/supplier-and-product/handler/product_handler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"context"
+	"github.com/TienMinh25/ecommerce-platform/internal/supplier-and-product/grpc/proto/partner_proto_gen"
+	"github.com/TienMinh25/ecommerce-platform/third_party/tracing"
+)
+
+func (p *PartnerHandler) GetProducts(ctx context.Context, data *partner_proto_gen.GetProductsRequest) (*partner_proto_gen.GetProductsResponse, error) {
+	ctx, span := p.tracer.StartFromContext(ctx, tracing.GetSpanName(tracing.HandlerLayer, "GetProducts"))
+	defer span.End()
+
+	res, err := p.productService.GetProducts(ctx, data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (p *PartnerHandler) GetProductByID(ctx context.Context, data *partner_proto_gen.GetProductDetailRequest) (*partner_proto_gen.GetProductDetailResponse, error) {
+	ctx, span := p.tracer.StartFromContext(ctx, tracing.GetSpanName(tracing.HandlerLayer, "GetProductByID"))
+	defer span.End()
+}
+
+func (p *PartnerHandler) GetProductReviewsByID(ctx context.Context, data *partner_proto_gen.GetProductReviewsRequest) (*partner_proto_gen.GetProductReviewsResponse, error) {
+
+}
